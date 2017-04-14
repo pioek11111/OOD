793b9929e9833fa3b7222f4f9a7e45b30c5065a9
Piotr Niedziela <piotrek11111@hotmail.com>
2017-04-14 23:58:53 +0200

    Add project files.

---
+++ PO_Lab02/PaintingRobot.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Robots
+{
+    public class PaintingRobot
+    {
+        public virtual double GetPaintedLength(double time)
+        {
+            return time;
+        }
+        public override string ToString()
+        {
+            return "basic robot: \n";
+        }
+    }
+
+    public abstract class RobotDecorator : PaintingRobot
+    {
+        protected PaintingRobot robot;
+
+        public RobotDecorator(PaintingRobot _robot)
+        {
+            robot = _robot;
+        }
+        public override double GetPaintedLength(double time)
+        {
+            return robot.GetPaintedLength(time);
+        }
+        public override string ToString()
+        {
+            return robot.ToString();
+        }
+    }
+    class RobotNTimes : RobotDecorator
+    {
+        int n;
+        public RobotNTimes(PaintingRobot r,int _n = 2):base(r)
+        {
+            n = _n;
+        }
+
+        public override double GetPaintedLength(double time)
+        {
+            return base.GetPaintedLength(time) * n;
+        }
+
+        public override string ToString()
+        {
+            base.ToString();
+            if (n == 2) return base.ToString() + "Two times faster Robot \n";
+            else return base.ToString() + n.ToString() + " times faster Robot \n";
+        }
+    }
+    class RobotplusN : RobotDecorator
+    {
+        int n;
+        public RobotplusN(PaintingRobot r, int n1 = 1):base(r)
+        {
+            this.n = n1;
+        }
+
+        public override double GetPaintedLength(double time)
+        {
+            return base.GetPaintedLength(time) + n;
+        }
+        public override string ToString()
+        {
+            return base.ToString() + "Add " +n.ToString() +" Robot \n";
+        }
+    }
+    class RobotLog : RobotDecorator
+    {
+        public RobotLog(PaintingRobot r) : base(r)
+        {
+            
+        }
+
+        public override double GetPaintedLength(double time)
+        {
+            return base.GetPaintedLength(time) * Math.Log(time);
+        }
+        public override string ToString()
+        {
+            return base.ToString() + "N*log(N) Robot \n";
+        }
+    }
+    class Compaund : PaintingRobot
+    {
+        PaintingRobot[] robots;
+        PaintingRobot curr;
+        public Compaund(params PaintingRobot[] r)
+        {
+            robots = new PaintingRobot[r.GetLength(0)];
+            for (int i = 0; i < r.GetLength(0); i++)
+            {
+                robots[i] = r[i];
+            }
+        }
+        public override double GetPaintedLength(double time)
+        {
+            double max = 0;
+
+            foreach (var r in robots)
+            {
+                if (r.GetPaintedLength(time) > max)
+                {
+                    max = r.GetPaintedLength(time);
+                    curr = r;
+                }
+            }
+            return max;
+        }
+        public override string ToString()
+        {
+            StringBuilder s = new StringBuilder();
+            s.Append("Compound Max Robot\n");
+            foreach(var r in robots)
+            {
+                s.Append(r.ToString());
+            }
+            return s.ToString();          
+        }
+    }  
+}
---
+++ PO_Lab02/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Robots
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PaintingRobot[] robots = new PaintingRobot[5];
+            robots[0] = new PaintingRobot();
+            robots[1] = new RobotNTimes(robots[0]);
+            robots[2] = new RobotLog(robots[1]);
+            robots[3] = new RobotplusN(robots[2]);
+            robots[4] = new Compaund(robots[1], robots[2], robots[3]);
+
+            foreach (var robot in robots)
+            {
+                Console.WriteLine($"Current robot: {robot}");
+                Console.WriteLine($"Painted length in 2 time units: {robot.GetPaintedLength(2)}.");
+                Console.WriteLine($"Painted length in 3 time units: {robot.GetPaintedLength(3)}.");
+                Console.WriteLine("");
+            }
+
+            PaintingRobot robot2 = new PaintingRobot();            
+            robot2 = new RobotNTimes(robot2);
+            robot2 = new RobotplusN(robot2,10);
+            robot2 = new RobotNTimes(robot2,3);
+
+            Console.WriteLine($"Current robot: {robot2}");
+            Console.WriteLine($"Painted length in 2 time units: {robot2.GetPaintedLength(2)}.");
+            Console.WriteLine($"Painted length in 3 time units: {robot2.GetPaintedLength(3)}.");
+            Console.WriteLine("");
+        }
+    }
+}
